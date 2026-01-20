318ab2fe7eb4f438d3f4a9aebdfb40dbd89b99ff
ewaterbury <ericwaterbury70@gmail.com>
2026-01-20 10:22:11 -0800

    test: added tests for Gameboard class

--- __test__/gameboard.test.js
+++ __test__/gameboard.test.js
@@ -1,7 +1,10 @@
 const Gameboard = require('/modules/gameboard.js');
 const Ship = require('/modules/ship.js');
 
-//Mocks Ship Class
+beforeEach(() => {
+    jest.clearAllMocks();
+});
+
 jest.mock('/modules/ship.js', () => {
     return jest.fn().mockImplementation(() => ({
         getPosition: jest.fn(),
@@ -10,15 +13,24 @@ jest.mock('/modules/ship.js', () => {
     }));
 });
 
-//Generate Instance of Mock
 const mockShip = new Ship();
 
-test('RevieveAttack Test', () => {
+test('RevieveAttack [no hit] Test', () => {
     const gameboard = new Gameboard();
     gameboard.newShip(mockShip);
     mockShip.getPosition.mockReturnValue([0, 1, 2, 3, 4]);
-    expect(gameboard.recieveAttack(1, 1)).toBeFalsy();
-    expect(gameboard.recieveAttack(0, 0)).toBe(mockShip);
+    gameboard.recieveAttack(1, 1);
+    expect(mockShip.hit).toHaveBeenCalledTimes(0);
+});
+
+test('RevieveAttack [no hit - hit] Test', () => {
+    const gameboard = new Gameboard();
+    gameboard.newShip(mockShip);
+    mockShip.getPosition.mockReturnValue([0, 1, 2, 3, 4]);
+    gameboard.recieveAttack(1, 1);
+    expect(mockShip.hit).toHaveBeenCalledTimes(0);
+    gameboard.recieveAttack(0, 0);
+    expect(mockShip.hit).toHaveBeenCalledTimes(1);
 });
 
 test('fleetSunk Test', () => {
@@ -27,4 +39,14 @@ test('fleetSunk Test', () => {
     mockShip.isSunk.mockReturnValueOnce(true).mockReturnValueOnce(false);
     expect(gameboard.fleetSunk()).toBe(true);
     expect(gameboard.fleetSunk()).toBe(false);
+});
+
+test('queryStrike Test', () => {
+     const gameboard = new Gameboard();
+     gameboard.newShip(mockShip);
+     gameboard.recieveAttack(0, 0);
+     gameboard.recieveAttack(1, 1);
+     expect(gameboard.queryStrike(0,0)).toBe(true);
+     expect(gameboard.queryStrike(1,1)).toBe(false);
+     expect(gameboard.queryStrike(2,2)).toBe(null);
 });
