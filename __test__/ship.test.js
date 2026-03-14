3d0fceb60331ac9023d482639696d15ba612d110
ewaterbury <ericwaterbury70@gmail.com>
2026-03-14 11:58:06 -0700

    refactor: improved isolation and clarity

--- __test__/ship.test.js
+++ __test__/ship.test.js
@@ -1,15 +1,20 @@
 const Ship = require('/modules/ship.js');
 
-const position = [0, 1, 2, 3, 4];
-const ship = new Ship(position);
+test('hit/isSunk Test', () => {
+    const position = [0, 1, 2, 3, 4];
+    const ship = new Ship(position);
 
-test('getPosition() Test', () => {
-    expect(ship.getPosition()).toBe(position);
-});
-
-test('isSunk/hit() Test', () => {
-    for (let i = 0; i < 5; i++){
+    for (let i = 0; i < position.length - 1; i++){
+        expect(ship.isSunk()).toBe(false);
         ship.hit();
-        i < 4 ? expect(ship.isSunk()).toBe(false) : expect(ship.isSunk()).toBe(true);
     }
+
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
 });
+
+test('getPosition Test', () => {
+    const position = [0];
+    const ship = new Ship(position);
+    expect(ship.getPosition()).toEqual(position);
+})
