3c94fcae7ef2e5e54f78c1d8bfbcbe47528c8292
ewaterbury <ericwaterbury70@gmail.com>
2026-04-03 11:26:49 -0700

    feat: applied formatting.

--- modules/utilities.js
+++ modules/utilities.js
@@ -1,18 +1,18 @@
 class Utilities {
-  // Generate random number, inclusive.
-  static randomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-
-  // Randomly shuffles array (Fisher–Yates shuffle).
-  static shuffleInPlace(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const random = Utilities.randomInt(0, i);
-      [array[i], array[random]] = [array[random], array[i]];
+    // Generate random number, inclusive.
+    static randomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
     }
 
-    return array;
-  }
+    // Randomly shuffles array (Fisher–Yates shuffle).
+    static shuffleInPlace(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const random = Utilities.randomInt(0, i);
+            [array[i], array[random]] = [array[random], array[i]];
+        }
+
+        return array;
+    }
 }
 
 module.exports = Utilities;
