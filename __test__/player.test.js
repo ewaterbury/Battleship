2414c356bda23b6b1e6efd83ba3c85d413b07a8b
ewaterbury <ericwaterbury70@gmail.com>
2026-04-21 11:13:07 -0700

    refactor: updated test suite to account for changes in module.

--- __test__/player.test.js
+++ __test__/player.test.js
@@ -1,50 +1,92 @@
-const Player = require("/modules/player.js");
-const Gameboard = require("/modules/gameboard.js");
-let mockGameboard;
-
-beforeEach(() => {
-    jest.clearAllMocks();
-    mockGameboard = new Gameboard();
-});
-
 jest.mock("/modules/gameboard.js", () => {
     return jest.fn().mockImplementation(() => ({
         addShip: jest.fn(),
         receiveAttack: jest.fn(),
         fleetSunk: jest.fn(),
         queryCell: jest.fn(),
+        queryBoard: jest.fn(),
     }));
 });
 
-test("setFleet Test", () => {
-    const player = new Player(mockGameboard);
-    const fleet = [[1], [2], [3], [4], [5]];
-    player.setFleet(fleet);
-    expect(mockGameboard.addShip).toHaveBeenCalledTimes(fleet.length);
-});
+const Player = require("/modules/player.js");
+const Gameboard = require("/modules/gameboard.js");
 
-test("receiveAttack Test", () => {
-    const player = new Player(mockGameboard);
-    player.receiveAttack(0);
-    expect(mockGameboard.receiveAttack).toHaveBeenCalledTimes(1);
-});
+const BOARD_SIZE = 10;
+const FLEET = [[1], [2], [3], [4], [5]];
 
-test("gameOver Test", () => {
-    const player = new Player(mockGameboard);
-    mockGameboard.fleetSunk
-        .mockReturnValueOnce(true)
-        .mockReturnValueOnce(false);
-    expect(player.gameOver()).toBe(true);
-    expect(player.gameOver()).toBe(false);
-});
+let mockGameboard;
+let player;
 
-test("queryCell test", () => {
-    const player = new Player(mockGameboard);
-    mockGameboard.queryCell
-        .mockReturnValueOnce(null)
-        .mockReturnValueOnce(true)
-        .mockReturnValueOnce(false);
-    expect(player.queryCell(0)).toBe(null);
-    expect(player.queryCell(0)).toBe(true);
-    expect(player.queryCell(0)).toBe(false);
+describe("Player", () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+        mockGameboard = new Gameboard(BOARD_SIZE);
+        player = new Player(mockGameboard, FLEET);
+    });
+
+    describe("Initialization", () => {
+        test("Calls addShip once for each ship in fleet", () => {
+            expect(mockGameboard.addShip).toHaveBeenCalledTimes(FLEET.length);
+        });
+    });
+
+    describe("receiveAttack", () => {
+        test("Calls gameboard.receiveAttack with given index", () => {
+            player.receiveAttack(0);
+            expect(mockGameboard.receiveAttack).toHaveBeenCalledTimes(1);
+            expect(mockGameboard.receiveAttack).toHaveBeenCalledWith(0);
+        });
+    });
+
+    describe("gameOver", () => {
+        test("Returns true when fleet is sunk", () => {
+            mockGameboard.fleetSunk.mockReturnValueOnce(true);
+            expect(player.gameOver()).toBe(true);
+        });
+
+        test("Returns false when fleet is not sunk", () => {
+            mockGameboard.fleetSunk.mockReturnValueOnce(false);
+            expect(player.gameOver()).toBe(false);
+        });
+    });
+
+    describe("queryCell", () => {
+        const empty = null;
+        const hit = "hit";
+        const miss = "miss";
+        const sunk = "sunk";
+
+        test("Returns null for non-attacked cell", () => {
+            mockGameboard.queryCell.mockReturnValueOnce(empty);
+            expect(player.queryCell(0)).toBe(null);
+        });
+
+        test("Returns 'hit' on cell holding hit ship", () => {
+            mockGameboard.queryCell.mockReturnValueOnce(hit);
+            expect(player.queryCell(0)).toBe("hit");
+        });
+
+        test("Returns 'miss' on attacked cell w/ no ship", () => {
+            mockGameboard.queryCell.mockReturnValueOnce(miss);
+            expect(player.queryCell(0)).toBe("miss");
+        });
+
+        test("Returns 'sunk' on cell holding sunk ship", () => {
+            mockGameboard.queryCell.mockReturnValueOnce(sunk);
+            expect(player.queryCell(0)).toBe("sunk");
+        });
+    });
+
+    describe("queryBoard", () => {
+        test("Returns correct gameboard", () => {
+            const board = Array.from(
+                { length: BOARD_SIZE ** 2 },
+                (_, i) => null,
+            );
+
+            mockGameboard.queryBoard.mockReturnValueOnce(board);
+
+            expect(player.queryBoard()).toBe(board);
+        });
+    });
 });
