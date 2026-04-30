b05902e09cfde763799ea2e602bced005982f50a
ewaterbury <ericwaterbury70@gmail.com>
2026-04-29 18:27:21 -0700

    feat: added utilities module for view helpers.

---
+++ modules/mvc/view/view-utilities.js
@@ -0,0 +1,14 @@
+export default class ViewUtilities {
+    // Capitalize the first letter of a string.
+    static capitalize(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    // Convert cell to battleship-style pattern (e.g., 4 -> A4, 21 -> B1)
+    static getCellName(cell, boardsize) {
+        const A_CHAR = 65;
+        const row = Math.floor(cell / boardsize);
+        const col = cell % boardsize ? cell % boardsize : boardsize;
+        return String.fromCharCode(A_CHAR + row) + col;
+    }
+}
