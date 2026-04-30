ac2389d53649337bb09c6bbd74519b90f6b5d9c2
ewaterbury <ericwaterbury70@gmail.com>
2026-04-29 17:56:08 -0700

    feat: added EL constants for DOM element types.

--- modules/constants.js
+++ modules/constants.js
@@ -1,2 +1,7 @@
-const CELL = { HIT: "hit", MISS: "miss", EMPTY: null, SUNK: "sunk" };
-export default CELL;
+export const CELL = { HIT: "hit", MISS: "miss", EMPTY: null, SUNK: "sunk" };
+export const EL = {
+    DIV: "div",
+    H3: "h3",
+    SECTION: "section",
+    SPAN: "span",
+};
