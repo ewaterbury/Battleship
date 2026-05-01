939a04a6c4fee3099015de202fa50784fc4445f2
ewaterbury <ericwaterbury70@gmail.com>
2026-04-30 18:23:05 -0700

    feat: added logTurn method to class.

--- modules/mvc/view/logger-view.js
+++ modules/mvc/view/logger-view.js
@@ -1,4 +1,5 @@
 import { EL } from "../../constants.js";
+import Utils from "./view-utilities.js";
 
 export default class LoggerView {
     #parentSelector;
@@ -7,6 +8,7 @@ export default class LoggerView {
     constructor(parentSelector) {
         this.#parentSelector = parentSelector;
         this.#id = "log-area";
+        this.#log = null;
         this.#addGameLog();
     }
 
@@ -23,6 +25,35 @@ export default class LoggerView {
         const log = document.createElement(EL.OL);
         container.append(header, log);
         document.querySelector(this.#parentSelector).after(container);
+    }
+
+    logTurn(summary) {
+        // Log Entry
+        const entry = document.createElement("li");
+
+        // Message
+        const message = document.createElement("p");
+        const msgStart = document.createElement("span");
+        msgStart.textContent = `Turn ${summary.turn}: ${Utils.capitalize(summary.player)} attacked  ${Utils.getCellName(summary.cell, summary.boardsize)} [ `;
+        const hitStatus = document.createElement("span");
+        const hitType =
+            summary.status === "hit" || summary.status === "sunk"
+                ? "hit"
+                : "miss";
+        hitStatus.textContent = hitType;
+        hitStatus.classList.add(hitType);
+        const msgEnd = document.createElement("span");
+        msgEnd.textContent = " ]";
+        message.append(msgStart, hitStatus, msgEnd);
+
+        if (summary.shipSunk) {
+            const shipSunk = document.createElement("span");
+            shipSunk.textContent = ` [ Size ${summary.shipSunk} ship sunk ]`;
+            message.append(shipSunk);
+        }
+
+        entry.append(message);
+        document.querySelector("#log-area ol").append(entry);
     }
 
     delete() {
