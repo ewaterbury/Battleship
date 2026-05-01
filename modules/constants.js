f1a7be062b277bc92482c97ee2c33557b6969dd2
ewaterbury <ericwaterbury70@gmail.com>
2026-04-30 18:25:13 -0700

    feat: added ordered list and list items to constants library.

--- modules/constants.js
+++ modules/constants.js
@@ -2,6 +2,8 @@ export const CELL = { HIT: "hit", MISS: "miss", EMPTY: null, SUNK: "sunk" };
 export const EL = {
     DIV: "div",
     H3: "h3",
+    OL: "ol",
+    LI: "li",
     SECTION: "section",
     SPAN: "span",
 };
