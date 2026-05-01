cb3d675cf516e0a4ab9f66859cfd16c072f1f44b
ewaterbury <ericwaterbury70@gmail.com>
2026-05-01 11:16:03 -0700

    feat: added button, menu, and paragraph to constants library.

--- modules/constants.js
+++ modules/constants.js
@@ -1,9 +1,12 @@
 export const CELL = { HIT: "hit", MISS: "miss", EMPTY: null, SUNK: "sunk" };
 export const EL = {
+    BUTTON: "button",
     DIV: "div",
     H3: "h3",
-    OL: "ol",
     LI: "li",
+    MENU: "menu",
+    OL: "ol",
+    P: "p",
     SECTION: "section",
     SPAN: "span",
 };
