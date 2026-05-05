69b34fd9881ac58a2f62ef1f097a6d5d21ddd2a7
ewaterbury <ericwaterbury70@gmail.com>
2026-05-05 10:56:36 -0700

    refactor: incorporated makeElement helper method from View-Utilties.

--- modules/mvc/view/gameboard-view.js
+++ modules/mvc/view/gameboard-view.js
@@ -21,42 +21,60 @@ export default class GameboardView extends ViewComponent {
         this.addClass("gameboard");
 
         // Build board caption.
-        const label = document.createElement(EL.H3);
-        label.textContent = Utils.capitalize(player);
+        const label = Utils.makeElement({
+            type: EL.H3,
+            text: Utils.capitalize(player),
+        });
 
         // Build layout spacer (Formatting only).
-        const corner = document.createElement(EL.DIV);
-        corner.classList.add("corner");
+        const corner = Utils.makeElement({
+            type: EL.DIV,
+            classList: "corner",
+        });
 
         // Build column Labels (Top row of board).
-        const colLabels = document.createElement(EL.DIV);
-        colLabels.classList.add("col-labels");
+        const colLabels = Utils.makeElement({
+            type: EL.DIV,
+            classList: "col-labels",
+        });
 
         for (let col = 1; col <= boardsize; col++) {
-            const cell = document.createElement(EL.SPAN);
-            cell.textContent = col;
-            colLabels.append(cell);
+            // Build cells for colLabels.
+            colLabels.append(
+                Utils.makeElement({
+                    type: EL.SPAN,
+                    text: col,
+                }),
+            );
         }
 
         // Build row labels (Left column of board).
-        const rowLabels = document.createElement(EL.DIV);
-        rowLabels.classList.add("row-labels");
+        const rowLabels = Utils.makeElement({
+            type: EL.DIV,
+            classList: "row-labels",
+        });
 
         for (let row = 0; row < boardsize; row++) {
-            const cell = document.createElement(EL.SPAN);
             const A_CHAR = 65;
-            cell.textContent = String.fromCharCode(A_CHAR + row);
-            rowLabels.append(cell);
+            rowLabels.append(
+                Utils.makeElement({
+                    type: EL.SPAN,
+                    text: String.fromCharCode(A_CHAR + row),
+                }),
+            );
         }
 
         // Build board grid.
-        const boardGrid = document.createElement(EL.DIV);
-        boardGrid.classList.add("board-grid");
+        const boardGrid = Utils.makeElement({
+            type: EL.DIV,
+            classList: "board-grid",
+        });
 
         for (let cellNum = 1; cellNum <= totalCells; cellNum++) {
-            const cell = document.createElement(EL.DIV);
-
-            cell.textContent = Utils.getCellName(cellNum, boardsize);
+            const cell = Utils.makeElement({
+                type: EL.DIV,
+                text: Utils.getCellName(cellNum, boardsize),
+            });
 
             cell.dataset.cell = cellNum;
             cell.dataset.player = player;
