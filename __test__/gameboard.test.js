bec45843d1aa17061b0063d79b5c246041b11261
ewaterbury <ericwaterbury70@gmail.com>
2026-05-13 21:48:05 -0700

    refactor: updated test structure to align with other test suits.

--- __test__/gameboard.test.js
+++ __test__/gameboard.test.js
@@ -1,11 +1,9 @@
 import Gameboard from "../modules/gameboard.js";
-
+const STD_BOARD = 10;
 const H = "hit";
 const E = null;
 const M = "miss";
 const S = "sunk";
-
-let mockShip;
 
 const createMockShip = () => ({
     getPosition: jest.fn(),
@@ -13,78 +11,97 @@ const createMockShip = () => ({
     isSunk: jest.fn(),
 });
 
-beforeEach(() => {
-    jest.clearAllMocks();
-    mockShip = createMockShip();
+describe("receiveAttack", () => {
+    let mockShip;
+
+    beforeEach(() => {
+        mockShip = createMockShip();
+    });
+
+    test("Does not call ship.hit() on miss, then calls it on hit.", () => {
+        const gameboard = new Gameboard(STD_BOARD);
+        gameboard.addShip(mockShip);
+        mockShip.getPosition.mockReturnValue([0, 1, 2, 3, 4]);
+        gameboard.receiveAttack(11);
+        expect(mockShip.hit).not.toHaveBeenCalled();
+        gameboard.receiveAttack(0);
+        expect(mockShip.hit).toHaveBeenCalledTimes(1);
+    });
+
+    test("queryCell returns miss on miss", () => {
+        const gameboard = new Gameboard(STD_BOARD);
+        gameboard.receiveAttack(11);
+        expect(gameboard.queryCell(11)).toBe(M);
+    });
+
+    test("queryCell returns hit on hit", () => {
+        const gameboard = new Gameboard(STD_BOARD);
+        gameboard.addShip(mockShip);
+        mockShip.getPosition.mockReturnValue([1]);
+        gameboard.receiveAttack(1);
+        expect(gameboard.queryCell(1)).toBe(H);
+    });
+
+    test("queryCell returns hit on hit and sunk when target sunk", () => {
+        const gameboard = new Gameboard(STD_BOARD);
+        gameboard.addShip(mockShip);
+        mockShip.getPosition.mockReturnValue([0, 1]);
+        mockShip.isSunk.mockReturnValueOnce(false).mockReturnValueOnce(true);
+        gameboard.receiveAttack(0);
+        expect(gameboard.queryCell(0)).toBe(H);
+        gameboard.receiveAttack(1);
+        expect(gameboard.queryCell(0)).toBe(S);
+        expect(gameboard.queryCell(1)).toBe(S);
+    });
 });
 
-test("receiveAttack [no hit] Test", () => {
-    const gameboard = new Gameboard();
-    gameboard.addShip(mockShip);
-    mockShip.getPosition.mockReturnValue([0, 1, 2, 3, 4]);
-    gameboard.receiveAttack(11);
-    expect(mockShip.hit).toHaveBeenCalledTimes(0);
+describe("fleetSunk", () => {
+    test("Returns false when single ship is not sunk", () => {
+        const mockShip = createMockShip();
+        const gameboard = new Gameboard(STD_BOARD);
+        gameboard.addShip(mockShip);
+        mockShip.isSunk.mockReturnValueOnce(false);
+        expect(gameboard.fleetSunk()).toBe(false);
+    });
+
+    test("Returns true when single ship is sunk", () => {
+        const mockShip = createMockShip();
+        const gameboard = new Gameboard(STD_BOARD);
+        gameboard.addShip(mockShip);
+        mockShip.isSunk.mockReturnValueOnce(true);
+        expect(gameboard.fleetSunk()).toBe(true);
+    });
+
+    test("Returns false when no ships are sunk", () => {
+        const gameboard = new Gameboard(STD_BOARD);
+        const ships = [createMockShip(), createMockShip(), createMockShip()];
+        ships.forEach((ship) => ship.isSunk.mockReturnValue(false));
+        ships.forEach((ship) => gameboard.addShip(ship));
+        expect(gameboard.fleetSunk()).toBe(false);
+    });
+
+    test("Returns false when some ships are sunk", () => {
+        const gameboard = new Gameboard(STD_BOARD);
+        const ships = [createMockShip(), createMockShip(), createMockShip()];
+        ships[0].isSunk.mockReturnValue(true);
+        ships[1].isSunk.mockReturnValue(false);
+        ships[2].isSunk.mockReturnValue(false);
+        ships.forEach((ship) => gameboard.addShip(ship));
+        expect(gameboard.fleetSunk()).toBe(false);
+    });
+
+    test("Returns true when all ships are sunk", () => {
+        const gameboard = new Gameboard(STD_BOARD);
+        const ships = [createMockShip(), createMockShip(), createMockShip()];
+        ships.forEach((ship) => ship.isSunk.mockReturnValue(true));
+        ships.forEach((ship) => gameboard.addShip(ship));
+        expect(gameboard.fleetSunk()).toBe(true);
+    });
 });
 
-test("receiveAttack [no hit - hit] Test", () => {
-    const gameboard = new Gameboard();
-    gameboard.addShip(mockShip);
-    mockShip.getPosition.mockReturnValue([0, 1, 2, 3, 4]);
-    gameboard.receiveAttack(11);
-    expect(mockShip.hit).toHaveBeenCalledTimes(0);
-    gameboard.receiveAttack(0);
-    expect(mockShip.hit).toHaveBeenCalledTimes(1);
-});
-
-test("fleetSunk Test [one ship]", () => {
-    const gameboard = new Gameboard();
-    gameboard.addShip(mockShip);
-    mockShip.isSunk.mockReturnValueOnce(true).mockReturnValueOnce(false);
-    expect(gameboard.fleetSunk()).toBe(true);
-    expect(gameboard.fleetSunk()).toBe(false);
-});
-
-test("fleetSunk Test [multiple ships]", () => {
-    const gameboard = new Gameboard();
-    gameboard.addShip(mockShip);
-    mockShip.isSunk.mockReturnValueOnce(false).mockReturnValueOnce(true);
-    expect(gameboard.fleetSunk()).toBe(false);
-    mockShip.isSunk.mockReturnValueOnce(true).mockReturnValueOnce(true);
-    expect(gameboard.fleetSunk()).toBe(true);
-});
-
-test("queryCell Test [empty]", () => {
-    const gameboard = new Gameboard();
-    gameboard.addShip(mockShip);
-    mockShip.getPosition.mockReturnValue([0, 1]);
-    gameboard.receiveAttack(1);
-    expect(gameboard.queryCell(11)).toBe(E);
-});
-
-test("queryCell Test [no hit]", () => {
-    const gameboard = new Gameboard();
-    gameboard.addShip(mockShip);
-    mockShip.getPosition.mockReturnValue([0, 1]);
-    gameboard.receiveAttack(11);
-    expect(gameboard.queryCell(11)).toBe(M);
-});
-
-test("queryCell Test [hit]", () => {
-    const gameboard = new Gameboard();
-    gameboard.addShip(mockShip);
-    mockShip.getPosition.mockReturnValue([0, 1]);
-    gameboard.receiveAttack(1);
-    expect(gameboard.queryCell(1)).toBe(H);
-});
-
-test("queryCell Test [hit - sunk]", () => {
-    const gameboard = new Gameboard();
-    gameboard.addShip(mockShip);
-    mockShip.getPosition.mockReturnValue([0, 1]);
-    mockShip.isSunk.mockReturnValueOnce(false).mockReturnValueOnce(true);
-    gameboard.receiveAttack(0);
-    expect(gameboard.queryCell(0)).toBe(H);
-    gameboard.receiveAttack(1);
-    expect(gameboard.queryCell(0)).toBe(S);
-    expect(gameboard.queryCell(1)).toBe(S);
+describe("queryCell", () => {
+    test("queryCell returns null on empty cell", () => {
+        const gameboard = new Gameboard(STD_BOARD);
+        expect(gameboard.queryCell(1)).toBe(E);
+    });
 });
