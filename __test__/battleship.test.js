2b6894ffb931cbf375d459a4edfd1a5bd9b8c97e
ewaterbury <ericwaterbury70@gmail.com>
2026-05-18 15:53:50 -0700

    test: updated battleship test suite for recent changes to class.

--- __test__/battleship.test.js
+++ __test__/battleship.test.js
@@ -1,35 +1,69 @@
+// |------ Player Mocks Setup ------|
 // Mocked methods used by the Player class.
 const createMockController = () => ({
     receiveAttack: jest.fn(),
     gameOver: jest.fn(),
     queryCell: jest.fn(),
     queryBoard: jest.fn(),
+    getSunkShip: jest.fn(),
 });
 
-// Mocked methods used by the player's and computer's mocked Player classes.
-const mockPlayerController = createMockController();
-const mockComputerController = createMockController();
+// Stores mocked methods used by the player's and computer's mocked Player classes.
+let mockPlayer;
+let mockComputer;
 
-// Mocked methods used by the computer's mocked AttackLogic class.
-const mockAttackLogic = { getAttack: jest.fn() };
+// Helper, sets gameOver return values for Player mocks.
+const mockGameOver = (player, computer) => {
+    mockPlayer.gameOver.mockReturnValue(player);
+    mockComputer.gameOver.mockReturnValue(computer);
+};
 
 // Replaces the Player class with a Jest mock constructor.
-// Mock implementations are assigned in before each through Player.mockImplementation.
+// Mock implementations are assigned in before each through Player.mockImplementation block.
 jest.mock("/modules/player.js", () => {
     return jest.fn();
 });
 
-// Replaces AttackLogic with mock implementation each time the class is initialized.
-jest.mock("/modules/computer-logic/attack-logic.js", () => {
-    return jest.fn().mockImplementation(() => mockAttackLogic);
+// |------ Log Mock Setup ------|
+const createMockLog = () => {
+    let latestValue = null;
+    let mockedLog = null;
+
+    return {
+        get latest() {
+            return latestValue;
+        },
+
+        set latest(value) {
+            latestValue = value;
+        },
+
+        get log() {
+            return mockedLog;
+        },
+
+        set log(arr) {
+            mockedLog = arr;
+        },
+
+        addEntry: jest.fn(),
+    };
+};
+
+let mockLog;
+
+// Replaces the Log class with a Jest mock constructor.
+jest.mock("../modules/log", () => {
+    return jest.fn();
 });
 
 // Import modules (Must be done after mocks).
 import Battleship from "/modules/battleship.js";
 import Player from "/modules/player.js";
 import AttackLogic from "/modules/computer-logic/attack-logic.js";
+import Log from "../modules/log";
 
-// Constants
+// Testing Constants
 const BOARD_SIZE = 10;
 const PLAYER_FIRST = 0.9;
 const COMPUTER_FIRST = 0.1;
@@ -41,143 +75,160 @@ const FLEET_TEMPLATE = [
     [81, 82, 83, 84, 85],
 ];
 
-//Holds battleship class for each test.
-let battleship;
-
-// Helper, sets gameOver return values for Player mocks.
-const mockGameOver = (player, computer) => {
-    mockPlayerController.gameOver.mockReturnValue(player);
-    mockComputerController.gameOver.mockReturnValue(computer);
-};
+let battleship; // Holds Battleship instance.
 
 beforeEach(() => {
     // Clear mock useage data, keep mocks.
     jest.clearAllMocks();
+
+    mockPlayer = createMockController();
+    mockComputer = createMockController();
+    mockLog = createMockLog();
 
     // Set mock value for Math.random to make turn order deterministic.
     jest.spyOn(Math, "random").mockReturnValue(PLAYER_FIRST);
 
     // Callback assigns correct mock when Player classes are initialized.
     // Player instance is initialized first, Computer is initialized second.
-    // Mock implementations are introduced in the same order.
+    // Mock implementations are assigned in the same order.
     let playersMocked = 0; // Tracks implementation order.
     Player.mockImplementation(() => {
         playersMocked++;
-        return playersMocked === 1
-            ? mockPlayerController
-            : mockComputerController;
+        return playersMocked === 1 ? mockPlayer : mockComputer;
     });
 
-    // Mock return value of queryBoard to prevent breaking attackLogic Module.
-    mockPlayerController.queryBoard.mockReturnValue([]);
+    // Mocks return value of queryBoard (Prevents breaking of attackLogic Module).
+    mockPlayer.queryBoard.mockReturnValue([]);
+
+    Log.mockImplementation(() => mockLog);
 
     // Battleship is initialized.
-    // Mocks are initialized.
     battleship = new Battleship(BOARD_SIZE, FLEET_TEMPLATE);
 });
 
-describe("getState", () => {
-    test("Gets inital game state", () => {
-        mockGameOver(false, false);
-        expect(battleship.getState()).toEqual({
-            attacker: "player",
-            defender: "computer",
-            gameOver: false,
-            winner: null,
+describe("getters", () => {
+    describe("latestTurn", () => {
+        test("Returns arbitrary game turn", () => {
+            mockLog.latest = {
+                turn: 50,
+                attacker: "computer",
+                defender: "player",
+                cell: 50,
+                status: "miss",
+                sunkShip: 0,
+                gameOver: false,
+            };
+
+            expect(battleship.latestTurn).toEqual({
+                turn: 50,
+                attacker: "computer",
+                defender: "player",
+                cell: 50,
+                status: "miss",
+                sunkShip: 0,
+                gameOver: false,
+            });
         });
     });
 
-    test("Updates attacker/defender status after turn ends", () => {
-        mockGameOver(false, false);
-        battleship.newTurn();
+    describe("log", () => {
+        test("Returns arbitrary log", () => {
+            const myLog = [
+                {
+                    turn: 1,
+                    attacker: "computer",
+                    defender: "player",
+                    cell: 1,
+                    status: "miss",
+                    sunkShip: 0,
+                    gameOver: false,
+                },
+                {
+                    turn: 2,
+                    attacker: "player",
+                    defender: "computer",
+                    cell: 2,
+                    status: "miss",
+                    sunkShip: 0,
+                    gameOver: false,
+                },
+                {
+                    turn: 3,
+                    attacker: "computer",
+                    defender: "player",
+                    cell: 3,
+                    status: "miss",
+                    sunkShip: 0,
+                    gameOver: false,
+                },
+            ];
 
-        expect(battleship.getState()).toEqual({
-            attacker: "computer",
-            defender: "player",
-            gameOver: false,
-            winner: null,
+            mockLog.log = myLog;
+            expect(battleship.log).toEqual(myLog);
         });
-    });
-
-    test("Gets winner and shows game is over (player wins)", () => {
-        mockGameOver(false, true);
-        expect(battleship.getState()).toEqual({
-            attacker: "player",
-            defender: "computer",
-            gameOver: true,
-            winner: "player",
-        });
-    });
-
-    test("Gets winner and shows game is over  (computer wins)", () => {
-        mockGameOver(true, false);
-        expect(battleship.getState()).toEqual({
-            attacker: "player",
-            defender: "computer",
-            gameOver: true,
-            winner: "computer",
-        });
-    });
-});
-
-describe("queryBoard", () => {
-    test("Returns player board on query", () => {
-        const state = battleship.getState();
-
-        battleship.queryBoard(state.attacker);
-
-        expect(mockPlayerController.queryBoard).toHaveBeenCalledTimes(1);
-        expect(mockComputerController.queryBoard).not.toHaveBeenCalled();
-    });
-
-    test("Returns computer board on query", () => {
-        const state = battleship.getState();
-
-        const initialCallsPlayer =
-            mockPlayerController.queryBoard.mock.calls.length;
-
-        const initialCallsComputer =
-            mockComputerController.queryBoard.mock.calls.length;
-
-        battleship.queryBoard(state.defender);
-
-        expect(mockPlayerController.queryBoard).toHaveBeenCalledTimes(
-            initialCallsPlayer,
-        );
-        expect(mockComputerController.queryBoard).toHaveBeenCalledTimes(
-            initialCallsComputer + 1,
-        );
-    });
-
-    test("Throws error on invalid id", () => {
-        expect(() => battleship.queryBoard()).toThrow("Invalid player id");
     });
 });
 
 describe("newTurn", () => {
-    test("Attacker and defender swap on new turn", () => {
+    test("Attacker/defender and turn count increments", () => {
         mockGameOver(false, false);
+        mockComputer.queryCell.mockReturnValue("miss");
+        mockPlayer.queryCell.mockReturnValue("miss");
+        battleship.logTurn(1);
 
-        expect(battleship.getState().attacker).toBe("player");
-        expect(battleship.getState().defender).toBe("computer");
+        expect(mockLog.addEntry).toHaveBeenCalledWith(
+            1,
+            "player",
+            "computer",
+            1,
+            "miss",
+            0,
+            false,
+            null,
+        );
 
         battleship.newTurn();
+        battleship.logTurn(2);
 
-        expect(battleship.getState().attacker).toBe("computer");
-        expect(battleship.getState().defender).toBe("player");
+        const callToInspect = 2;
+
+        expect(mockLog.addEntry).toHaveBeenNthCalledWith(
+            callToInspect,
+            2,
+            "computer",
+            "player",
+            2,
+            "miss",
+            0,
+            false,
+            null,
+        );
+    });
+
+    test("Winner detected on change of turn", () => {
+        mockGameOver(false, true);
+        mockComputer.queryCell.mockReturnValue("sunk");
+        mockComputer.getSunkShip.mockReturnValue(5);
+        battleship.logTurn(1);
+        expect(mockLog.addEntry).toHaveBeenCalledWith(
+            1,
+            "player",
+            "computer",
+            1,
+            "sunk",
+            5,
+            true,
+            "player",
+        );
     });
 });
 
 describe("sendAttack", () => {
     describe("Player Attack", () => {
-        test("recieveAttack called on valid attack", () => {
+        test("receiveAttack called on valid attack", () => {
             battleship.sendAttack(1);
-            expect(mockComputerController.receiveAttack).toHaveBeenCalledWith(
-                1,
-            );
-            expect(mockComputerController.receiveAttack).toHaveBeenCalledTimes(
-                1,
-            );
+            expect(mockComputer.receiveAttack).toHaveBeenCalledWith(1);
+            expect(mockComputer.receiveAttack).toHaveBeenCalledTimes(1);
         });
 
         test("Error thrown on empty attack", () => {
@@ -210,44 +261,59 @@ describe("sendAttack", () => {
             let playersMocked = 0;
             Player.mockImplementation(() => {
                 playersMocked++;
-                return playersMocked === 1
-                    ? mockPlayerController
-                    : mockComputerController;
+                return playersMocked === 1 ? mockPlayer : mockComputer;
             });
 
             // Mock return value of queryBoard to prevent breaking attackLogic Module.
-            mockPlayerController.queryBoard.mockReturnValue([]);
+            mockPlayer.queryBoard.mockReturnValue([]);
 
             // Battleship is re-initialized.
             // Mocks are re-initialized.
             battleship = new Battleship(BOARD_SIZE, FLEET_TEMPLATE);
         });
 
-        test("recieveAttack called on valid attack", () => {
-            mockAttackLogic.getAttack.mockReturnValue(1);
-
-            battleship.sendAttack();
-            expect(mockPlayerController.receiveAttack).toHaveBeenCalledWith(1);
-            expect(mockPlayerController.receiveAttack).toHaveBeenCalledTimes(1);
+        test("receiveAttack called on valid attack", () => {
+            battleship.sendAttack(1);
+            expect(mockPlayer.receiveAttack).toHaveBeenCalledWith(1);
+            expect(mockPlayer.receiveAttack).toHaveBeenCalledTimes(1);
         });
 
         test("Error thrown on empty attack", () => {
-            mockAttackLogic.getAttack.mockReturnValue();
             expect(() => battleship.sendAttack()).toThrow(
                 "Attack is not a number",
             );
         });
 
         test("Error thrown on out-of-bounds attack", () => {
-            mockAttackLogic.getAttack.mockReturnValue(-1);
-            expect(() => battleship.sendAttack()).toThrow(
+            expect(() => battleship.sendAttack(-1)).toThrow(
                 "Attack is out of bounds",
             );
 
-            mockAttackLogic.getAttack.mockReturnValue(100);
-            expect(() => battleship.sendAttack()).toThrow(
+            expect(() => battleship.sendAttack(100)).toThrow(
                 "Attack is out of bounds",
             );
         });
+    });
+});
+
+describe("logTurn", () => {
+    test("logTurn calls Log.addEntry with correct arguments", () => {
+        mockGameOver(false, false);
+        mockComputer.queryCell.mockReturnValue("miss");
+        mockPlayer.queryCell.mockReturnValue("miss");
+        battleship.logTurn(1);
+
+        expect(mockLog.addEntry).toHaveBeenCalledWith(
+            1,
+            "player",
+            "computer",
+            1,
+            "miss",
+            0,
+            false,
+            null,
+        );
+
+        battleship.newTurn();
     });
 });
