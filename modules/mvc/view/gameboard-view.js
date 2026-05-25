77fa5258a38871e46eef9f8bccc54bb253e5dcf5
ewaterbury <ericwaterbury70@gmail.com>
2026-05-25 10:35:58 -0700

    refactor: updated import names for cross-module consistency.

--- modules/mvc/view/gameboard-view.js
+++ modules/mvc/view/gameboard-view.js
@@ -1,8 +1,8 @@
 import { EL } from "../../constants.js";
 import Utils from "./view-utilities.js";
-import ViewComponent from "./view-component.js";
+import Component from "./view-component.js";
 
-export default class GameboardView extends ViewComponent {
+export default class GameboardView extends Component {
     constructor(player, boardsize) {
         // Vaildate player input.
         if (typeof player !== "string" || player.trim() === "")
@@ -29,37 +29,35 @@ export default class GameboardView extends ViewComponent {
         this.addClass("gameboard");
 
         // Build board caption.
-        const label = new ViewComponent(EL.H3).setText(
-            Utils.capitalize(player),
-        );
+        const label = new Component(EL.H3).setText(Utils.capitalize(player));
 
         // Build layout spacer (Formatting only).
-        const corner = new ViewComponent(EL.DIV).addClass("corner");
+        const corner = new Component(EL.DIV).addClass("corner");
 
         // Build column Labels (Top row of board).
-        const colLabels = new ViewComponent(EL.DIV).addClass("col-labels");
+        const colLabels = new Component(EL.DIV).addClass("col-labels");
 
         for (let col = 1; col <= boardsize; col++)
             // Build cells for colLabels.
-            colLabels.append(new ViewComponent(EL.SPAN).setText(col));
+            colLabels.append(new Component(EL.SPAN).setText(col));
 
         // Build row labels (Left column of board).
-        const rowLabels = new ViewComponent(EL.DIV).addClass("row-labels");
+        const rowLabels = new Component(EL.DIV).addClass("row-labels");
 
         for (let row = 0; row < boardsize; row++) {
             const A_CHAR = 65;
             rowLabels.append(
-                new ViewComponent(EL.SPAN).setText(
+                new Component(EL.SPAN).setText(
                     String.fromCharCode(A_CHAR + row),
                 ),
             );
         }
 
         // Build board grid.
-        const boardGrid = new ViewComponent(EL.DIV).addClass("board-grid");
+        const boardGrid = new Component(EL.DIV).addClass("board-grid");
 
         for (let cellNum = 1; cellNum <= totalCells; cellNum++) {
-            const cell = new ViewComponent(EL.DIV)
+            const cell = new Component(EL.DIV)
                 .setText(Utils.getCellName(cellNum, boardsize))
                 .addDataset("cell", cellNum)
                 .addDataset("player", player)
