0ab783616fc5f8b2152baa5fd82133bd68987531
ewaterbury <ericwaterbury70@gmail.com>
2026-05-27 13:13:09 -0700

    refactor: added comments, validation, and renamed isPlaying method.

--- modules/mvc/view/audio/audio-loop-component.js
+++ modules/mvc/view/audio/audio-loop-component.js
@@ -1,45 +1,56 @@
 import AudioComponent from "./audio-component.js";
 
 export default class AudioLoop extends AudioComponent {
-    #loop;
-    #loopInterval;
+    #interval; // Interval in milliseconds between loops.
+    #timer = null; // Stores timeout while loop is running (Initialized as null).
+    #tick; // Recursive callback used to loop audio.
 
-    #timer;
-    #tick;
-
-    constructor(id, src, loopInterval) {
+    constructor(id, src, interval) {
+        // Initialize base AudioComponent.
         super(id, src);
-        this.#loopInterval = loopInterval;
 
-        this.#timer = null;
+        if (!Number.isFinite(interval) || interval <= 0)
+            throw new TypeError(
+                "Loop interval must be a positive finite number",
+            );
 
+        this.#interval = interval;
+
+        // Recursive timeout callback.
         this.#tick = () => {
+            // Plays audio.
             this.play();
-            this.#timer = setTimeout(this.#tick, this.#loopInterval);
+
+            // Queue next loop.
+            this.#timer = setTimeout(this.#tick, this.#interval);
         };
     }
 
-    // Overwite original play method with arrow function to preserve this on setInterval callback.
+    // Overwrite original play method with arrow function to preserve this inside setTimeout.
     play = () => {
         super.play();
     };
 
     startLoop() {
-        // If no loop, call setInterval with this.play as callback.
+        // Start recursive timeout loop if not already running.
         if (!this.#timer) this.#tick();
 
         return this;
     }
 
     stopLoop() {
+        // Stop loop and set #timer to null.
         clearTimeout(this.#timer);
         this.#timer = null;
+
+        // Pause current audio loop.
         this.pause();
 
         return this;
     }
 
-    isPlaying() {
+    isLooping() {
+        // Return whether loop is active.
         return this.#timer !== null;
     }
 }
