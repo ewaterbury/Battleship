79943becbcff1597c0eff5a930f998a71d91327e
ewaterbury <ericwaterbury70@gmail.com>
2026-06-03 13:44:13 -0700

    refactor: clean up module structure and readability.

--- modules/mvc/view/settings/theme/theme-setting.js
+++ modules/mvc/view/settings/theme/theme-setting.js
@@ -1,10 +1,18 @@
+// Core Components
 import Component from "../../view-component.js";
-import ThemeToggle from "./theme-toggle.js";
+
+// Element Library
 import { EL } from "../../../../constants.js";
+
+// Imported Components
+import ThemeToggle from "./theme-toggle.js"; // Toggle button for changing page theme.
 
 export default class ThemeSetting extends Component {
     constructor() {
+        // Initialize container (li) using super constructor.
         super(EL.LI);
+
+        // |----- UI Construction -----|
         this.append(new Component(EL.H3).setText("Toggle Theme:"));
         this.append(new ThemeToggle());
     }
