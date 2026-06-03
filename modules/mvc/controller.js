d9bbcf470dbc9b4e12797724219e42ec9e26c7ff
ewaterbury <ericwaterbury70@gmail.com>
2026-06-03 08:56:43 -0700

    refactor: major refactor from previous version.

--- modules/mvc/controller.js
+++ modules/mvc/controller.js
@@ -1,76 +1,100 @@
 // Model Modules
 import Model from "./model/model.js";
 
-// View Modules
-import GameboardView from "./view/gameboard-view.js";
-import LogView from "./view/log/logger-view.js";
-import MenuView from "./view/menu/menu-view.js";
+// Top Level View Modules
+import GameView from "./view/game-view.js";
 
-// View Component
-import Component from "./view/view-component.js";
-
-import ThemeButton from "./view/buttons/themeButton.js";
+// Audio Components
+import AudioComponent from "./view/audio/audio-component.js";
+import AudioLoop from "./view/audio/audio-loop-component.js";
 
 export default class Controller {
+    // Game Model
     #model;
 
-    // Gamboard views
-    #playerView;
-    #computerView;
-    #logView;
-    #menuView;
+    // Views
+    #activeView;
 
-    // Mount Targets
-    #gameArea;
-    #gameSidebar;
+    #boardsize = 10;
 
     constructor() {
         // Initialize game model.
         this.#model = new Model();
 
-        // Mount Targets.
-        this.#gameArea = document.getElementById("game-area");
-        this.#gameSidebar = document.getElementById("game-sidebar");
+        // Initialize effects audio.
+        this.gameEffects = {
+            hit: new AudioComponent("hit", "./audio/hit.mp3"),
+            miss: new AudioComponent("miss", "./audio/miss.wav"),
+            sunk: new AudioComponent("sunk", "./audio/sunk.wav"),
+        };
+
+        // Initialize backing audio.
+        this.backingAudio = new AudioLoop(
+            "sonar",
+            "./audio/ping.wav",
+            2800,
+        ).startLoop();
     }
 
-    setUp() {}
-
-    newGame(boardsize) {
-        // Build and mount gameboards.
-        this.#playerView = new GameboardView("player", boardsize);
-        this.#computerView = new GameboardView("computer", boardsize);
-        [this.#playerView, this.#computerView].forEach((board) =>
-            board.mount(this.#gameArea),
-        );
-
-        // Build and mount log and menu.
-        this.#logView = new LogView(boardsize);
-        this.#menuView = new MenuView();
-
-        this.#menuView.addSettingsButton(new ThemeButton());
-
-        [(this.#logView, this.#menuView)].forEach((component) =>
-            component.mount(this.#gameSidebar),
-        );
-
-        // Add buttons
+    get boardsize() {
+        return this.#boardsize;
     }
 
-    // ---------- Log ----------
-    logTurn(turn) {
-        this.#logView.logTurn(turn);
-        return this;
+    // Application States
+    initializeApp() {
+        // |---------- Set Inital Theme ----------|
+
+        // Check for previously saved theme.
+        const savedTheme = localStorage.getItem("theme");
+
+        // Get prefered theme.
+        // Used saved theme if available.
+        // Then fall back to system preference.
+        // Then default to light.
+        const initialTheme = savedTheme
+            ? savedTheme
+            : window.matchMedia("(prefers-color-scheme: dark)").matches
+              ? "dark"
+              : "light";
+
+        // Apply theme to <html> element.
+        document.documentElement.setAttribute("data-theme", initialTheme);
+
+        // Save prefered theme in local storage.
+        localStorage.setItem("theme", initialTheme);
     }
 
-    // ---------- Menu ----------
-    addGameOption(button) {
-        // this.#view.gameMenu.append(button);
+    // |----- Game State Initialization Methods -----|
+
+    startPreGame() {}
+
+    startGame() {
+        // Start new game in model.
+        this.#model.newGame();
+
+        // Call update view with initilized GameView.
+        this.#updateView(new GameView(boardsize, this));
     }
 
-    addSetting(button) {
-        this.#menuView.addSettingsButton(button);
+    startPostGame() {}
+
+    #updateView(view) {
+        // Clear current view.
+        if (this.#activeView) this.#activeView.remove();
+
+        // Set active view to passed.
+        this.#activeView = view;
     }
 
-    // ---------- Helpers ----------
-    makeButton() {}
+    // |----- Log -----|
+    sendlog() {
+        // Get turn from model.
+        // Send turn to View.
+        this.#activeView.logTurn(this.#model.latest);
+    }
+
+    // |----- Audio -----|
+    effect(status) {
+        this.gameEffects[status].play();
+    }
 }
