a6fc5fe5526e9390943756e212b12609a79093eb
ewaterbury <ericwaterbury70@gmail.com>
2026-06-03 15:34:08 -0700

    refactor: clean up module structure and readability.

--- modules/mvc/view/button.js
+++ modules/mvc/view/button.js
@@ -1,11 +1,18 @@
+// Core Components
 import Component from "./view-component.js";
+
+// Element Library
 import { EL } from "./../../constants.js";
 
 export default class Button extends Component {
-    constructor(id, callback) {
+    constructor(id, onClick) {
+        // Initialize container (button) using super constructor.
         super(EL.BUTTON, id);
+
+        // |----- Behavior -----|
+        // Attach click handler to button.
         this.element.addEventListener("click", (e) => {
-            callback(e);
+            onClick(e);
         });
     }
 }
