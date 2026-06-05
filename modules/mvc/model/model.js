1c77f51c5f3077afebdd40b65d833e562f97c086
ewaterbury <ericwaterbury70@gmail.com>
2026-06-05 10:00:23 -0700

    feat: added properties for board size and fleet composition.

--- modules/mvc/model/model.js
+++ modules/mvc/model/model.js
@@ -1,21 +1,60 @@
+// Default Model Values
+import { DEFAULT_VALUES } from "../../constants.js";
+
+// Model Components
 import Battleship from "./battleship.js";
 
 export default class Model {
     // Battleship Model
     #battleship;
 
-    // Game States
-    #preGame;
-    #inGame;
-    #postGame;
+    constructor() {
+        // Represents board size (e.g., 10 x 10).
+        this.boardSize = { current: DEFAULT_VALUES.BOARD_SIZE };
 
-    constructor() {}
+        // Set min and max board sizes as read-only fields.
+        Object.defineProperties(this.boardSize, {
+            min: { value: 7, writable: false },
+            max: { value: 12, writable: false },
+        });
+
+        // Represents fleet composition for the next game.
+        // Defaults to standard Battleship fleet [2, 3, 3, 4, 5].
+        this.fleetTemplate = {
+            carrier: {
+                size: DEFAULT_VALUES.CARRIER.SIZE,
+                count: DEFAULT_VALUES.CARRIER.COUNT,
+            },
+            battleship: {
+                size: DEFAULT_VALUES.BATTLESHIP.SIZE,
+                count: DEFAULT_VALUES.BATTLESHIP.COUNT,
+            },
+            cruiser: {
+                size: DEFAULT_VALUES.CRUISER.SIZE,
+                count: DEFAULT_VALUES.CRUISER.SIZE,
+            },
+            destroyer: {
+                size: DEFAULT_VALUES.DESTROYER.SIZE,
+                count: DEFAULT_VALUES.DESTROYER.COUNT,
+            },
+        };
+
+        // Set ship.size as read-only field.
+        for (const ship of Object.values(this.fleetTemplate))
+            Object.defineProperty(ship, "size", {
+                size: { value: ship.size, writable: false },
+            });
+    }
+
+    get latestTurn() {
+        return this.#battleship.latestTurn;
+    }
 
     // Initialize new game
-    newGame(boardsize, playerFleet) {
-        this.#battleship = new Battleship(boardsize, playerFleet);
+    newGame(playerFleet) {
+        this.#battleship = new Battleship(this.boardSize, playerFleet);
 
-        this.gameState = this.#inGame;
+        this.gameState = "ingame";
     }
 
     takeTurn(attack) {
