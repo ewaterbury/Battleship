139c6a8463b1861e15b0dbf1a433191a0f15a7b6
ewaterbury <ericwaterbury70@gmail.com>
2026-06-12 13:18:04 -0700

    refactor: updated HTML structure of component/subcomponents.

--- modules/mvc/view/setup/setup-options/setup-options.js
+++ modules/mvc/view/setup/setup-options/setup-options.js
@@ -1,28 +1,36 @@
 // Core Components
-import Component from "../../view-component.js";
+import ViewComponent from "../../view-component.js";
 
 // Elements Library
 import { EL } from "../../../../constants.js";
 
-// Import components.
+// Import Components.
 import BoardSizeIncrementer from "./board-size-incrementer.js"; // Menu item to specify board size.
-import FleetComposer from "./fleet-comp/fleet-comp.js"; // Submenu to specfiy fleet composition.
+import FleetComposer from "./fleet-composer/fleet-composer-component.js"; // Submenu to specfiy fleet composition.
 import Button from "../../button.js"; // Button used to reset fleet size and board size to default.
 
-export default class SetupOptions extends Component {
+export default class SetupOptions extends ViewComponent {
     constructor(controller) {
         // Initialize root element (section) and assign ID using super constructor.
         super(EL.SECTION, "setup-options-area");
 
         // |----- UI Construction -----|
         // Contoller is passed to relevant components during initialization.
-        this.append(new Component(EL.H2).setText("Setup Options:"));
-        this.append(new BoardSizeIncrementer(controller));
-        this.append(new FleetComposer(controller));
-        this.append(
-            new Button().setText("Reset Game Options"),
-            this.#resetSetup,
+        this.append(new ViewComponent(EL.H2).setText("Setup Options:"));
+
+        const menu = new ViewComponent(EL.MENU);
+        menu.append(new BoardSizeIncrementer(controller));
+        menu.append(new FleetComposer(controller));
+
+        menu.append(
+            new ViewComponent(EL.LI).append(
+                new Button("reset-setup", this.#resetSetup).setText(
+                    "Reset Setup Options",
+                ),
+            ),
         );
+
+        this.append(menu);
     }
 
     #resetSetup = () => {};
