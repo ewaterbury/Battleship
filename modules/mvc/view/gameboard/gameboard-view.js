a729e4d6224d22309a041f6ea9a1d96882f5d93a
ewaterbury <ericwaterbury70@gmail.com>
2026-06-12 15:09:56 -0700

    refactor: removed validation and updated comments for cross-project consistency.

--- modules/mvc/view/gameboard/gameboard-view.js
+++ modules/mvc/view/gameboard/gameboard-view.js
@@ -1,74 +1,21 @@
-import { EL } from "../../../constants.js";
-import Utils from "../view-utilities.js";
+// Core Components
 import Component from "../view-component.js";
 
+// Elements Library
+import { EL } from "../../../constants.js";
+
+// Imported Components
+import Gameboard from "./gameboard-component.js";
+
 export default class GameboardView extends Component {
-    constructor(player, boardsize) {
-        // Vaildate player input.
-        if (typeof player !== "string" || player.trim() === "")
-            throw new TypeError("player must be non-empty string");
+    constructor(boardsize, controller) {
+        // Initialize container (section) and assign ID using super constructor.
+        super(EL.SECTION, "gameboard-area");
 
-        // Validate boardsize input.
-        if (typeof boardsize !== "number")
-            throw new TypeError("boardzise must be number");
+        // |----- UI Construction -----|
+        this.player = new Gameboard("player", boardsize, controller);
+        this.computer = new Gameboard("computer", boardsize, controller);
 
-        // Initialize 'root' using super constructor.
-        super(EL.SECTION, `${player.toLowerCase()}_board`);
-
-        // Build gameboard.
-        this.#buildBoard(boardsize, player);
-    }
-
-    #buildBoard(boardsize, player) {
-        const totalCells = boardsize ** 2;
-
-        // Set boardsize on stylesheet.
-        document.documentElement.style.setProperty("--board-size", boardsize);
-
-        // Build board container (Cached for repeat access).
-        this.addClass("gameboard");
-
-        // Build board caption.
-        const label = new Component(EL.H3).setText(Utils.capitalize(player));
-
-        // Build layout spacer (Formatting only).
-        const corner = new Component(EL.DIV).addClass("corner");
-
-        // Build column Labels (Top row of board).
-        const colLabels = new Component(EL.DIV).addClass("col-labels");
-
-        for (let col = 1; col <= boardsize; col++)
-            // Build cells for colLabels.
-            colLabels.append(new Component(EL.SPAN).setText(col));
-
-        // Build row labels (Left column of board).
-        const rowLabels = new Component(EL.DIV).addClass("row-labels");
-
-        for (let row = 0; row < boardsize; row++) {
-            const A_CHAR = 65;
-            rowLabels.append(
-                new Component(EL.SPAN).setText(
-                    String.fromCharCode(A_CHAR + row),
-                ),
-            );
-        }
-
-        // Build board grid.
-        const boardGrid = new Component(EL.DIV).addClass("board-grid");
-
-        for (let cellNum = 1; cellNum <= totalCells; cellNum++) {
-            const cell = new Component(EL.DIV)
-                .setText(Utils.getCellName(cellNum, boardsize))
-                .addDataset("cell", cellNum)
-                .addDataset("player", player)
-                .addDataset("state", "empty");
-
-            boardGrid.append(cell);
-        }
-
-        // Append components in order.
-        [label, corner, colLabels, rowLabels, boardGrid].forEach((component) =>
-            this.append(component),
-        );
+        this.append(this.player).append(this.computer);
     }
 }
