c9a4d1fadc377b9bb6dbb1e0d4eef27e8bc20611
ewaterbury <ericwaterbury70@gmail.com>
2026-06-16 12:44:03 -0700

    initial commit for default-settings component.

---
+++ modules/mvc/view/setup/setup-options/default-settings-component.js
@@ -0,0 +1,31 @@
+// Core Components
+import ViewComponent from "../../view-component.js";
+
+// Elements Library
+import { EL } from "../../../../constants.js";
+
+// Imported Componenets
+import Button from "../../button.js";
+
+export default class DefaultSettings extends ViewComponent {
+    #controller;
+
+    constructor(controller) {
+        // Initialize root element (li) using super constructor.
+        super(EL.LI);
+
+        this.#controller = controller;
+
+        // |----- UI Construction -----|
+        this.append(
+            new Button("reset-setup", this.#resetOnClick).setText(
+                "Reset to defaults",
+            ),
+        );
+    }
+
+    // Callback to reset game settings.
+    #resetOnClick = () => {
+        this.#controller.resetGameSettings();
+    };
+}
