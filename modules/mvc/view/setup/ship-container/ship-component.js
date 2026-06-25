73edfd7f3614d3517d3818dddb4c35c5d4d0032a
ewaterbury <ericwaterbury70@gmail.com>
2026-06-25 14:40:35 -0700

    feat: added method to select ship.

--- modules/mvc/view/setup/ship-container/ship-component.js
+++ modules/mvc/view/setup/ship-container/ship-component.js
@@ -5,12 +5,39 @@ import ViewComponent from "../../view-component.js";
 import { EL } from "../../../../constants.js";
 
 export default class Ship extends ViewComponent {
-    constructor(ship, count) {
-        super(EL.DIV, `${ship.type}-${count}`, "draggable");
+    #controller;
 
-        this.setAttr("draggable", true);
+    constructor(controller, ship) {
+        // Initialize root element (div), assign ID, and add class using super constructor.
+        super(EL.DIV, `${ship.type}-${ship.id}`, "dataset").addClass("ship");
+
+        this.#controller = controller;
+
+        // Add values to ship element.
+        this.addDataset("type", ship.type)
+            .addDataset("size", ship.size)
+            .addDataset("countId", ship.id);
+
+        if (ship.selected) this.addClass("selected");
 
         for (let i = 0; i < ship.size; i++)
-            this.append(new ViewComponent(EL.DIV));
+            this.append(
+                new ViewComponent(EL.DIV, "", "dataset").addDataset(
+                    "shipIndex",
+                    i,
+                ),
+            );
+
+        this.on("click", this.selectShip);
     }
+
+    selectShip = () => {
+        const data = this.readProp("dataset");
+
+        this.#controller.selectShip({
+            type: data.type,
+            size: Number(data.size),
+            id: Number(data.countId),
+        });
+    };
 }
