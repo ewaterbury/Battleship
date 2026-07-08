57e3720cd7d49a6533fb6733c86a4c5f5e087765
ewaterbury <ericwaterbury70@gmail.com>
2026-07-07 18:11:36 -0700

    refactor: renamed imported components for cross-project consistency.

--- modules/mvc/view/setup/setup-options/fleet-composer/fleet-composer-component.js
+++ modules/mvc/view/setup/setup-options/fleet-composer/fleet-composer-component.js
@@ -1,5 +1,5 @@
 // Core Components
-import Component from "../../../view-component.js";
+import ViewComponent from "../../../view-component.js";
 
 // Elements Library
 import { EL } from "../../../../../constants.js";
@@ -7,15 +7,17 @@ import { EL } from "../../../../../constants.js";
 // Imported Components
 import ShipIncrementer from "./ship-incrementer-component.js"; // Menu item to specify ship count for a ship type.
 
-export default class FleetComposer extends Component {
+export default class FleetComposer extends ViewComponent {
     constructor(controller) {
         // Initialize root element (li) and assign ID using super constructor.
         super(EL.LI, `fleet-incrementer`);
 
         // |----- UI Construction -----|
-        const form = new Component(EL.FORM);
-        const legend = new Component(EL.LEGEND).setText("Fleet Composition:");
-        const shipList = new Component(EL.UL);
+        const form = new ViewComponent(EL.FORM);
+        const legend = new ViewComponent(EL.LEGEND).setText(
+            "Fleet Composition:",
+        );
+        const shipList = new ViewComponent(EL.UL);
 
         // Add instance of ShipIncrementer for each ship.
         Object.values(controller.fleetTemplate).forEach((ship) =>
